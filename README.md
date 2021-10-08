b5fff6653805efc9e3a5bdf1b7078475d1da2f0d
Shigma <shigma10826@gmail.com>
2021-10-08 19:24:04 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# koishi-plugin-assets-smms
+ 
+[![npm](https://img.shields.io/npm/v/koishi-plugin-assets-smms?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-assets-smms)
+
+使用免费图床服务 https://sm.ms 存储 Koishi 接收到的静态资源文件。
---
+++ src/index.ts
@@ -0,0 +1,59 @@
+import { Assets, Context, Schema, Requester } from 'koishi'
+import { createHash } from 'crypto'
+import FormData from 'form-data'
+
+declare module 'koishi' {
+  interface Modules {
+    'assets-smms': typeof import('.')
+  }
+}
+
+export const schema = Schema.object({
+  token: Schema.string('sm.ms 的访问令牌。').required(),
+})
+
+interface Config {
+  token: string
+}
+
+class SmmsAssets extends Assets {
+  types = ['image']
+
+  http: Requester
+
+  constructor(ctx: Context, public config: Config) {
+    super(ctx)
+    this.http = ctx.http.extend({
+      endpoint: 'https://sm.ms/api/v2',
+      headers: { authorization: config.token },
+    })
+  }
+
+  async upload(url: string, file: string) {
+    const buffer = await this.download(url)
+    const payload = new FormData()
+    payload.append('smfile', buffer, file || createHash('sha1').update(buffer).digest('hex'))
+    const data = await this.http('POST', '/upload', payload, payload.getHeaders())
+    if (data.code === 'image_repeated') {
+      return data.images
+    }
+    if (!data.data) {
+      const error = new Error(data.message)
+      return Object.assign(error, data)
+    }
+    return data.data.url
+  }
+
+  async stats() {
+    const data = await this.http('POST', '/profile')
+    return {
+      assetSize: data.data.disk_usage_raw,
+    }
+  }
+}
+
+export const name = 'smms'
+
+export function apply(ctx: Context, config: Config) {
+  ctx.assets = new SmmsAssets(ctx, config)
+}
